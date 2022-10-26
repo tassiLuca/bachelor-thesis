349eea06ff119f5ca7322961549b0a3bb12e2eac
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-10-26 12:43:08 +0200

    add listings

--- resources/code/TestAnalysis.java
+++ resources/code/TestAnalysis.java
@@ -1,7 +1,16 @@
 package org.examples;
 
-public class App {
-    public void main(String... args) {
-        System.out.println("Hello World");
+import java.util.Arrays;
+
+/**
+ * This is a sample class to demonstrate the tokenization process.
+ */
+public class Main {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Program arguments: " + Arrays.toString(args));
+        } else {
+            System.out.println("Hello world from Java!");
+        }
     }
 }
